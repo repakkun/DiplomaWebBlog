a39cd49aac9cc75d3958c6a25d46305edb72c265
rePakkun <cj.ilya@yandex.ru>
2022-05-17 21:38:57 +0300

    Add project files.

---
+++ WebBlog/Controllers/PostController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebBlog.Data;
+using WebBlog.Models;
+
+namespace WebBlog.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public PostController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<User> objList = _db.Users;
+            return View(objList);
+        }
+
+        public IActionResult CreatePost(User user)
+        {
+            return View();
+        }
+            
+    }
+}
